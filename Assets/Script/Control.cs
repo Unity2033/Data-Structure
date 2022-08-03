f40d4b582dd0596334cc41fe53c47dbfe3ef252d
ksw617 <82032086+Unity2033@users.noreply.github.com>
2022-08-03 17:44:01 +0900

    Health System

--- Assets/Script/Control.cs
+++ Assets/Script/Control.cs
@@ -1,30 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Control : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] LayerMask [] layer;
-    RaycastHit hit;
+    [SerializeField] int maxHealth;
+    [SerializeField] int currentHealth;
+    [SerializeField] int attack;
 
-    Animator animator;
+    [SerializeField] Slider Gauge;
+
+    private RaycastHit hit;
+    private Animator animator;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        maxHealth = currentHealth;
     }
 
     void Update()
     {
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        Gauge.value = (float)currentHealth / maxHealth;
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
-        Ray ray = new Ray(transform.position, transform.forward);
+        Ray ray = new Ray
+        (
+               new Vector3
+               (transform.position.x, 1, transform.position.z), transform.forward           
+        );
 
         if(Physics.Raycast(ray, out hit, 2.0f, layer[0]))
         {
             speed = 0.0f;
             animator.SetBool("Attack State", true);
+
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName("attack1"))
+            {
+                if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                {
+                    animator.Rebind();
+                    hit.transform.GetComponent<Control>().currentHealth -= attack;
+                }
+            }
+        }
+        else if(Physics.Raycast(ray, out hit, 2.0f, layer[1]))
+        {
+            speed = 0.0f;
+            animator.SetBool("Idle State", true);
+        }
+        else
+        {
+            speed = 2.0f;
+            animator.SetBool("Attack State", false);
+            animator.SetBool("Idle State", false);
         }
     }
 }
