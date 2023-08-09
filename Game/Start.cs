3553e3f02ffbd7da8df058d67c8686c0d2acddd4
Unity2033 <kileab@naver.com>
2023-08-09 21:14:13 +0900

    (2023.8.9) C# 프로젝트 생성

---
+++ Game/Start.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Start
+    {
+        public class Monster
+        {
+            private int attack = 10;
+
+            public void Attack()
+            {
+                Console.WriteLine("공격 " + attack + " 데미지 추가");
+            }
+        }
+
+        internal class Program
+        {
+            static void Information()
+            {
+                Console.WriteLine("Program 클래스의 정보");
+            }
+
+            static void Main(string[] args)
+            {
+                // Console.WriteLine : 출력하는 함수입니다.
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("Hello, World!");
+
+                Information();
+
+                Monster monster = new Monster();
+                monster.Attack();
+
+            }
+        }
+    }
+}
