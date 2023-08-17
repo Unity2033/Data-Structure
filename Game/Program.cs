85f96db90cb20666dfded21bc65788b6266d9086
Unity2033 <kileab@naver.com>
2023-08-17 21:44:57 +0900

    (2023.8.17) 예외 처리

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,110 +1,89 @@
-﻿using System.ComponentModel;
-
-namespace Game
+﻿namespace Game
 {
-    public class Monster
+    public class GameObject
     {
-        public int hp;
+        // Run-Time 상수
+        // 선언과 동시에 초기화를 하지 않아도 됩니다.
+        readonly int itemID;
 
-        protected void Move()
-        {
-            Console.WriteLine("몬스터 이동");
-        }
-    }
+        // Compile-Time 상수
+        // 선언과 동시에 초기화가 이루어져야 합니다.
+        const float pi = 3.14f;
 
-    public class Goblin : Monster
-    {
-        #region base 키워드
-          // 하위 클래스에서 상위 클래스의 속성과 기능을
-          // 접근할 수 있는 지정자입니다.
-        public Goblin()
+        public GameObject()
         {
-            hp = 100;
-            base.Move();
-        }
-        #endregion
-
-        new public void Move()
-        {
-            Console.WriteLine("고블린 이동");
-        }
-    }
-
-    public class Slime : Monster
-    {
-        public Slime()
-        {
-            hp = 125;
-        }
-    }
-
-    public class Skeleton : Monster
-    { 
-        public Skeleton()
-        {
-            hp = 200;
-        }
-    }
-
-    public class Damageable
-    { 
-        public void Damage(Monster monster)
-        {
-            monster.hp -= 10;
-        }         
+            // readonly는 생성자에서 단 한 번만 초기화가 이루어집니다.
+            itemID = 35641;
+        } 
     }
 
     internal class Program
     {
+        static void Function1()
+        {
+            Function2();
+        }
+
+        #region 스택 풀기
+           // 예외가 발생한 함수에서 예외가 처리되지 않으면,
+           // 상위 함수로 돌아가서 예외를 처리하는 과정입니다.
+        static void Function2()
+        {
+            throw new Exception("My Exception");      
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            #region 업 캐스팅
-            // Damageable damageable = new Damageable();
-            // 
-            // Goblin goblin = new Goblin();
-            // Slime slime = new Slime();
-            // Skeleton skeleton = new Skeleton();
-            // 
-            // Console.WriteLine("고블린의 체력 : " + goblin.hp);
-            // Console.WriteLine("슬라임의 체력 : " + slime.hp);
-            // Console.WriteLine("스켈레톤의 체력 : " + skeleton.hp);
-            // 
-            // damageable.Damage(goblin);
-            // damageable.Damage(slime);
-            // damageable.Damage(skeleton);
-            // 
-            // Console.WriteLine("고블린의 체력 : " + goblin.hp);
-            // Console.WriteLine("슬라임의 체력 : " + slime.hp);
-            // Console.WriteLine("스켈레톤의 체력 : " + skeleton.hp);
+            #region 예외 처리
+            //  int[] array = new int[3];
+            //  
+            //  try
+            //  {
+            //      // int x = 10;
+            //      // int y = 0;
+            //  
+            //      // int result = x / y;
+            //  
+            //      //for(int i = 0; i < 5; i++)
+            //      //{
+            //      //    array[i] = i;
+            //      //}
+            //  
+            //      Function1();
+            //  
+            //      Console.WriteLine("Console");
+            //  }
+            //  catch(Exception error)
+            //  {
+            //      Console.WriteLine(error);
+            //  }
+            //  finally
+            //  {
+            //      Console.WriteLine("Close");
+            //  }
             #endregion
 
-            #region 파일 입출력
-
-            // 파일 쓰기
-            // FileStream fileStream = new FileStream("data.txt", FileMode.Create);
-            // 
-            // StreamWriter streamWrite = new StreamWriter(fileStream);
-            // 
-            // streamWrite.Write("HELLO");
-            // streamWrite.Write(25);
-            // 
-            // streamWrite.Close();
-
-            // 파일 읽기
-            // FileStream file = new FileStream("data.txt", FileMode.Open, FileAccess.Read);
-            // StreamReader streamReader = new StreamReader(file);
-            // 
-            // Console.WriteLine(streamReader.ReadLine());
-            // 
-            // streamReader.Close();
+            #region 최소 공배수
+           // int x = int.Parse(Console.ReadLine());
+           // int y = int.Parse(Console.ReadLine());
+           //
+           // int result = 1;
+           //
+           // for (int i = 2; i <= x && i <= y; i++)
+           // {
+           //     if(x % i == 0 && y % i == 0)
+           //     {
+           //         result *= i;
+           //         x /= i;
+           //         y /= i;
+           //
+           //         i--;
+           //     }
+           // }
+           //
+           // Console.WriteLine(result * x * y);
             #endregion
-
-            int x = int.Parse((Console.ReadLine()));
-            int y = int.Parse((Console.ReadLine()));
-
-            
-
-
         }
     }
 }
