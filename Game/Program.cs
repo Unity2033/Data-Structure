11045c8996c89b7fef9170f09cedf78dbcca3d71
Unity2033 <kileab@naver.com>
2023-08-21 21:44:35 +0900

    (2023.8.21) 클래스 설계 원칙

--- Game/Program.cs
+++ Game/Program.cs
@@ -2,139 +2,118 @@
 
 namespace Game
 {
-    #region 형식 매개 변수
-    // 함수에 형식을 지정하지 않아도 호출 시에
-    // 형식만 지정하여 사용하는 매개 변수입니다.
-    class Deque<T> where T : class
+    public class Dog
     {
-        // where T : struct 
-        // T 형식을 값 형식만 받을 수 있도록 제한하는 것입니다.
+        private int age;
+        private string name;
 
-        // where T : class
-        // T 형식을 참조 형식으로만 받을 수 있도록 제한하는 것입니다.
-
-        // where T : new()
-        // T 형식을 기본 생성자가 있어야 하는 클래스로 받을 수 있도록 제한하는 것입니다.
-
-        // where T : 클래스 이름
-        // T 형식에 해당 클래스이거나 상속 받은 하위 클래스만 받을 수 있도록 제한하는 것입니다.
-
-        // where T : 인터페이스 이름
-        // T 형식은 인터페이스를 구현 클래스만 받을 수 있도록 제한하는 것입니다.
-
-
-        private T data;
-        // c++ template는 컴파일이 1번 일어나며, C# generic은
-        // 컴파일이 2번 일어납니다.
-
-        // c++ template은 사용하지 않으면 컴파일하지 않으며,
-        // c# generic은 사용하지 않더라도 그에 관련된 정보를 
-        // 저장하기 위한 metadata를 생성합니다.
-
-        public Deque()
+        public Dog()
         {
-            Console.WriteLine("Deque 생성");
+            age = 1;
+            name = "Alistar";
         }
 
-        public void Push(T data)
+        public void Eat()
         {
-            this.data = data;
+            Console.WriteLine("Eat");
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("Walk");
+        }
+
+        public string Representation()
+        {
+            return $"Name : {name}, Age : {age}";
         }
     }
-    #endregion
+
+    class Information
+    { 
+        public void Show(Dog dog)
+        {
+            Console.WriteLine(dog.Representation());
+        }    
+    }
+
+    public abstract class TerranUnit
+    {
+        public abstract void Speak();
+    }
+
+    public class Marine : TerranUnit
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("마린 생성");
+        }
+    }
+
+    public class FireBet : TerranUnit
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("파이어벳 생성");
+        }
+    }
+
+    public class Ghost : TerranUnit
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("고스트 생성");
+        }
+    }
+
+    public class Medic : TerranUnit
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("메딕 생성");
+        }
+    }
+
+    public class UnitManager
+    {
+        public void Create(TerranUnit unit)
+        {
+            unit.Speak();
+        }
+    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region 일반화 프로그래밍
-            // 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터 타입들을
-            // 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수 있는 프로그래밍 방식입니다.
-
-            // Deque<object> deque = new Deque<object>();
-
-            // deque.Push('A');
-            // deque.Push('B');
-
-            #region List
-            // List<int> list = new List<int>();
+            #region 단일 책임 원칙
+            // Module, class, function은 하나의 책임만 가져야 합니다.
+            // Dog dog = new Dog();
             // 
-            // list.Add(10);
-            // list.Add(20);
-            // list.Add(30);
-            // list.Add(40);
-            // list.Add(50);
-            // 
-            // Console.WriteLine(list.Capacity);
-            // 
-            // foreach (var element in list)
-            // {
-            //     Console.WriteLine(element);
-            // }
+            // Information info = new Information();
+            // info.Show(dog);
             #endregion
 
-            #region LinkedList
-            // LinkedList<int> linkedList = new LinkedList<int>();
-            // 
-            // linkedList.AddLast(1);
-            // linkedList.AddLast(2);
-            // linkedList.AddLast(3);
-            // 
-            // linkedList.AddFirst(6);
-            // linkedList.AddFirst(5);
-            // linkedList.AddFirst(4);
-            // 
-            // foreach (var element in linkedList)
-            // {
-            //     Console.WriteLine(element);
-            // }
-            // 
+            #region 개방 폐쇄 원칙
+            // Module, class, function은 확장에 대해 열려 있어야 하며,
+            // 수정에 대해서는 닫혀 있어야 합니다.
+
+            // UnitManager unitManager = new UnitManager();
+            //
+            // unitManager.Create(new Marine());
+            // unitManager.Create(new FireBet());
+            // unitManager.Create(new Ghost());
+            // unitManager.Create(new Medic());
             #endregion
 
-            #region Dictionary
-            /*
-            Dictionary<string, int> dictionary = new Dictionary<string, int>();
-            dictionary.Add("Sword", 10000);
-            dictionary.Add("Gloves", 50000);
-            dictionary.Add("Armor", 25000);
+            #region 인터페이스 분리 원칙
+             // 클라이언트는 자신이 사용하지 않는 함수에 대해
+             // 영향을 받지 않아야 하며, 인터페이스를 구체적이고 작은 단위의
+             // 역할로 인터페이스를 분리시켜 클라이언트가 꼭 필요한 함수만
+             // 이용할 수 있도록 해야합니다.
 
-            dictionary.Remove("Armor");
-
-            if (dictionary.ContainsKey("Armor"))
-            {
-                Console.WriteLine("KEY가 존재합니다.");
-            }
-            */
-            #endregion
-
-            #region Stack
-            /*
-            Stack<int> stack = new Stack<int>();
-
-            stack.Push(10);
-            stack.Push(20);
-            stack.Push(30);
-            stack.Push(40);
-
-            stack.Pop();
-
-            if (stack.Contains(40))
-            {
-                Console.WriteLine("값이 존재합니다.");
-            }
-            */
-            #endregion
-
-            #region 해시 테이블
-           // Hashtable hashTable = new Hashtable();
-           // hashTable.Add("One",10);
-           // hashTable.Add("Second",20);
-            #endregion
 
             #endregion
         }
     }
 }
-
-
-
