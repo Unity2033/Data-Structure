44a8038898607a57a2a5dd150c60c0a5bdbfcb74
Unity2033 <kileab@naver.com>
2023-08-24 21:50:17 +0900

    (2023.8.24) 인벤토리 시스템

---
+++ Game/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Inventory
+    {
+        uint lineX;
+        private Item[] items;
+
+        public Inventory(uint x, uint y)
+        {
+            lineX = x;
+            items = new Item[x * y];
+        }
+
+        public void AddItem(Item item)
+        {
+            for(int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == null)
+                {
+                    items[i] = item;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Game/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Item
+    {
+        private int price;
+        private string name;
+
+        public Item(int price, string name)
+        {
+            this.price = price;
+            this.name = name;
+        }
+    }
+}
