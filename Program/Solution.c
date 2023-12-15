bcef793c1eac96f15826d4eaafa27b0115d4fd22
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-12-15 21:47:45 +0900

    (2023.12.15) 주소 연산자

--- Program/Solution.c
+++ Program/Solution.c
@@ -2,45 +2,60 @@
 
 void main()
 {
-#pragma region 자료형 변환
-	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이
-	// 이루어질 때 기존에 지정했던 자료형을 다른 자료형으로
-	// 변환하는 과정입니다.
+#pragma region 주소 연산자
+	// 변수의 주소 값을 반환하는 연산자입니다.
 
-#pragma region 암묵적 형 변환
-	// 서로 다른 자료형으로 연산이 이루어질 때 자료형의
-	// 크기가 큰 자료형으로 변환되는 과정입니다.
+	// int data = 10;
 
-	//int integer = 10;
-	//float decimal = 6.5f;
+	//// %p : 메모리의 주솟값을 나타내는 연산자입니다.
+	// printf("data 변수의 주솟값 : %p\n", &data);
 
-	//printf("interger(정수형) + decimal(실수형) = %f\n", integer + decimal);
+	// 데이터의 주소 값은 해당 데이터가 저장된 메모리
+	// 의 시작 주소를 의미하며, 메모리의 공간은 1 byte
+	// 의 크기로 나누어 표현합니다.
 
-	//// 표현 범위가 작은 데이터에 표현범위가 큰 데이터를 저장하게 되면
-	//// 암묵적으로 데이터 손실이 발생하게 됩니다.
-
-	//integer = decimal;
-
-	//printf("interger변수의 값 = %d\n", integer);
-#pragma endregion
-
-#pragma region 명시적 형 변환
-	// 연산이 이루어지기 전에 사용자가 직접 자료형을
-	// 변환하는 과정입니다.
-
-	//int x = 5;
-	//int y = 2;
-
-	//float result = (float)x / y;
-
-	//printf("result 변수의 값 : %f\n", result);
-
-	// 정수형 변수끼리 연산을 수행하게 되면 정수의
-	// 결과값만 가질 수 있습니다.
-#pragma endregion
-
-
+	// 변수의 메모리 공간은 프로그램이 실행될 때마다
+	// 바뀌며, 여러 개의 변수가 있을 때 서로 고유의 
+	// 메모리 공간을 가지고 있습니다.
 
 #pragma endregion
+
+#pragma region scanf_s() 함수 
+	// 표준 입력 함수로, 여러 종류의 데이터를 다양한
+	// 서식에 맞추어 입력해주는 함수입니다.
+
+	//int count = 0;
+
+	//scanf_s("%d", &count);
+
+	//// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에
+	//// 데이터를 보관하였다가 입력하는 수간 버퍼 안의
+	//// 내용을 프로그램에 전송합니다.
+
+	//printf("count 변수의 값 : %d\n", count);
+
+	// 표준 입력 함수는 입력을 수행할 때까지 다음 
+	// 작업으로 넘어갈 수 없습니다.
+#pragma endregion
+
+#pragma region Star Tower
+
+	int count = 0;
+
+	scanf_s("%d", &count);
+
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+
+#pragma endregion
+
+
 
 }
