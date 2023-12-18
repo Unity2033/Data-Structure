140597a489cdb873302f269bec297842943da49b
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-12-18 21:46:56 +0900

    (2023.12.18) 포인터

---
+++ Class6th/Source.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+void main()
+{
+#pragma region 주소 연산자
+	// 변수의 주소 값을 반환하는 연산자입니다.
+
+	// int data = 10;
+
+	//// %p : 메모리의 주솟값을 나타내는 연산자입니다.
+	// printf("data 변수의 주솟값 : %p\n", &data);
+
+	// 데이터의 주소 값은 해당 데이터가 저장된 메모리
+	// 의 시작 주소를 의미하며, 메모리의 공간은 1 byte
+	// 의 크기로 나누어 표현합니다.
+
+	// 변수의 메모리 공간은 프로그램이 실행될 때마다
+	// 바뀌며, 여러 개의 변수가 있을 때 서로 고유의 
+	// 메모리 공간을 가지고 있습니다.
+
+#pragma endregion
+
+#pragma region scanf_s() 함수 
+	// 표준 입력 함수로, 여러 종류의 데이터를 다양한
+	// 서식에 맞추어 입력해주는 함수입니다.
+
+	//int count = 0;
+
+	//scanf_s("%d", &count);
+
+	//// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에
+	//// 데이터를 보관하였다가 입력하는 수간 버퍼 안의
+	//// 내용을 프로그램에 전송합니다.
+
+	//printf("count 변수의 값 : %d\n", count);
+
+	// 표준 입력 함수는 입력을 수행할 때까지 다음 
+	// 작업으로 넘어갈 수 없습니다.
+#pragma endregion
+
+#pragma region Star Tower
+
+	//int count = 0;
+
+	//scanf_s("%d", &count);
+
+	//for (int i = 0; i < count; i++)
+	//{
+	//	for (int j = 0; j <= i; j++)
+	//	{
+	//		printf("*");
+	//	}
+
+	//	printf("\n");
+	//}
+
+#pragma endregion
+}
--- Program/Solution.c
+++ Program/Solution.c
@@ -2,60 +2,64 @@
 
 void main()
 {
-#pragma region 주소 연산자
-	// 변수의 주소 값을 반환하는 연산자입니다.
+#pragma region 포인터
+	// 메모리의 주소 값을 저장할 수 있는 변수입니다.
 
-	// int data = 10;
+	//int data = 10;
+	//float pi = 3.14f;
 
-	//// %p : 메모리의 주솟값을 나타내는 연산자입니다.
-	// printf("data 변수의 주솟값 : %p\n", &data);
+	//// 8 byte 
+	//// [NULL]
 
-	// 데이터의 주소 값은 해당 데이터가 저장된 메모리
-	// 의 시작 주소를 의미하며, 메모리의 공간은 1 byte
-	// 의 크기로 나누어 표현합니다.
+	//int * ptr = NULL;
 
-	// 변수의 메모리 공간은 프로그램이 실행될 때마다
-	// 바뀌며, 여러 개의 변수가 있을 때 서로 고유의 
-	// 메모리 공간을 가지고 있습니다.
+	//// 포인터 변수도 자신의 메모리 공간을 가지고 있으며,
+	//// 포인터 변수에 변수의 주소를 저장하게 되면 해당 변수의
+	//// 시작 주소를 가리키게 됩니다.
 
-#pragma endregion
+	////     ptr 변수           
+	//// [data의 시작주소]	<---- data의 시작 주소
+	//ptr = &data;
 
-#pragma region scanf_s() 함수 
-	// 표준 입력 함수로, 여러 종류의 데이터를 다양한
-	// 서식에 맞추어 입력해주는 함수입니다.
+	//// 변수의 주소는 프로그램이 실행될 때마다 변경되며,
+	//// 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로
+	//// 포인터가 가리키는 메모리의 자료형으로 선언해주어야 합니다.
 
-	//int count = 0;
+	//printf("data 변수의 메모리 주소 : %p\n", &data);
+	//printf("ptr 변수의 값 : %p\n", ptr);
+	//printf("ptr 변수의 메모리 주소 : %p\n", &ptr);
 
-	//scanf_s("%d", &count);
+	//printf("data 변수의 값 : %d\n", data);
+	//printf("ptr 변수가 가리키는 값 : %d\n", *ptr);
 
-	//// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에
-	//// 데이터를 보관하였다가 입력하는 수간 버퍼 안의
-	//// 내용을 프로그램에 전송합니다.
+	//// 포인터 변수를 저장하기 위해 주소 값을 저장할 변수의 자료형과
+	//// 포인터 변수의 자료형이 일치해야 합니다.
 
-	//printf("count 변수의 값 : %d\n", count);
+	//ptr = &pi;
 
-	// 표준 입력 함수는 입력을 수행할 때까지 다음 
-	// 작업으로 넘어갈 수 없습니다.
-#pragma endregion
+	//printf("pi 변수의 값 : %f\n", pi);
+	//printf("ptr 변수가 가리키는 값 : %f\n", *ptr);
 
-#pragma region Star Tower
-
-	int count = 0;
-
-	scanf_s("%d", &count);
-
-	for (int i = 0; i < count; i++)
-	{
-		for (int j = 0; j <= i; j++)
-		{
-			printf("*");
-		}
-
-		printf("\n");
-	}
+	// 포인터 변수의 크기는 중앙 처리 장치가 한 번에
+	// 처리할 수 있는 크기로 정해지며, 한 번에 처리할 
+	// 수 있는 크기는 운영체제에 따라 크기가 결정됩니다.
 
 #pragma endregion
 
+#pragma region sizeof 연산자
+	// 피연산자의 크기를 바이트 단위로 반환하는 연산자입니다.
 
+	int * pointer = NULL;
+												
+	printf("char 자료형의 크기 : %d\n" ,   sizeof(char));
+	printf("short 자료형의 크기 : %d\n",   sizeof(short));
+	printf("int 자료형의 크기 : %d\n"  ,   sizeof(int));
+	printf("long 자료형의 크기 : %d\n" ,   sizeof(long));
+	printf("float 자료형의 크기 : %d\n" ,  sizeof(float));
+	printf("double 자료형의 크기 : %d\n",  sizeof(double));
+	printf("pointer 자료형의 크기 : %d\n", sizeof(pointer));
+
+
+#pragma endregion
 
 }
