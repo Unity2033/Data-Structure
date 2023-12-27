43340a3ae66c7baf468c6f7a0c31badfe3315c2d
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-12-27 21:45:17 +0900

    (2023.12.27) 동적 할당

--- Program/Solution.c
+++ Program/Solution.c
@@ -1,128 +1,90 @@
 #include <stdio.h>
-#include "Function.h" // int data;
-
-// 매크로의 경우 자료형이 존재하지 않으므로
-// 메모리 공간을 가지고 있지 않습니다.
-#define SIZE 100
-
-int level; // 전역 변수 (BSS 메모리 영역)
-           // 전역 변수는 직접 초기화하지 않으면 0 이라는 값으로 초기화됩니다.
-
-int stage = 1; // 전역 변수 (초기화가 이루어진 메모리 영역)
-
-void Stage()
-{
-	stage++;
-}
-
-void Increase()
-{
-	// 정적 변수는 단 한 번만 초기화가 이루어집니다.
-	static int score = 0;
-
-	score++;
-
-	printf("score 변수의 값 : %d\n", score);
-}
+#include <stdlib.h> // 메모리 관련 라이브러리
 
 int main()
 {
-#pragma region 전처리기
-	// 프로그램이 컴파일되기 이전에 프로그램에 대한
-	// 사전 처리를 하는 과정입니다.
+#pragma region 범용 포인터
+	// 자료형이 정해지지 않은 상태로 모든 자료형을
+	// 저장할 수 있는 포인터입니다.
 
-	// data = 100;
-
-	// printf("Function.h에서 생성한 data 변수의 값 : %d\n", data);
-
-	// 전처리기는 컴파일러가 아닌 선행처리기에 의해서
-	// 처리되는 문장이기 때문에 명령문 끝에 ";"을 사용하지 않습니다.
+	// int attack = 10;
+	// float health = 100.0f;
+	   
+	// void * vptr = NULL;
+	   
+	// vptr = &attack;
+	   
+	// // 범용 포인터는 메모리 주소에 접근해서 값을 변경할 수 없습니다.
+	// printf("vptr이 가리키는 값 : %d\n", *(int*)vptr);
+	   
+	// vptr = &health;
+	   
+	// printf("vptr이 가리키는 값 : %f\n", *(float*)vptr);
+	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가 가리키는
+	// 변수의 자료형으로 형 변환해야 합니다.
 #pragma endregion
 
-#pragma region 변수의 범위
+#pragma region 동적 할당
+	// 프로그램을 실행하는 중에 필요한 만큼 메모리를
+	// 할당하는 작업업니다.
 
-#pragma region 지역 변수
-    // 블록 내에서 선언된 변수로 블록 내에서만 유효하며,
-	// 블록이 종료되면 메모리에서 사라지는 특징을 가지고 있습니다.
+	//int * ptr = (int *)malloc(sizeof(int));
 
-	// { } 바깥에 있는 data 변수
-	//int data = 100;
+	//*ptr = 100;
+	//// 메모리를 동적 할당할 때 주소를 범용 포인터로
+	//// 반환하기 때문에 자료형을 변환한 다음 메모리에 
+	//// 할당해야 합니다.
 
+	//printf("*ptr이 가리키는 값 : %d\n", *ptr);
+
+	//free(ptr);
+	//// 동적으로 할당한 메모리는 힙 영역에 보관되어 있으므로
+	//// 사용이 끝나면 직접 해제 해주어야 합니다.
+
+	//// ptr [] -----> [999] [333] [] [] []
+	//ptr = malloc(sizeof(int) * 5);
+
+	//// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를
+ //   // 변경시킬 수 있으며, 동적으로 메모리의 크기를 할당할 
+ //   // 때 바이트 단위로 지정합니다.
+
+	//ptr[0] = 999;
+	//ptr[1] = 333;
+
+	//printf("ptr[0]가 가리키는 값 : %d\n", ptr[0]);
+	//printf("ptr[0]가 가리키는 값 : %d\n", ptr[1]);
+
+	//free(ptr);
+	// 메모리를 동적으로 할당한 상태에서 해제하지 않으면
+	// 메모리 누수 현상이 일어나며, 메모리 누수가 일어나게 되면
+	// 더 이상 메모리를 할당할 수 없는 상태가 됩니다.
+
+#pragma endregion
+
+#pragma region 단축 평가 계산
+	// 논리 연산에서 두 피연산자 중 왼쪽에 있는 결과가
+	// "거짓"이면 우측의 피연산자의 값은 평가하지 않고
+	// 바로 결과를 얻어내는 계산입니다.
+
+	//int x = 0;
+	//int y = 0;
+
+	//if (x < y && ++x)
 	//{
-	//	// { } 안에 있는 data 변수
-	//	int data = 999;
-	//	printf("{ } 안에 있는 data의 값 : %d\n", data);
+	//	printf("Short Curcuit\n");
 	//}
 
-	//printf("{ } 바깥에 있는 data의 값 : %d\n", data);
-#pragma endregion
+	//printf("x 변수의 값 : %d\n", x);
 
-#pragma region 전역 변수
-	// 프로그램의 어디에서나 접근할 수 있으며, 프로그램이
-	// 종료되어야만 메모리에서 사라지는 특징을 가지고 있습니다.
-
-	//Stage();
-
-	//printf("전역 변수 stage의 값 : %d\n", stage);
-	//
-	//stage = 10;
-
-	//printf("전역 변수 stage의 값 : %d\n", stage);
-#pragma endregion
-
-#pragma region 정적 변수
-	// 프로그램이 실행될 때 메모리에 올라가게 되며, 지역 변수의 특징과
-	// 전역 변수의 특징을 가지고 있는 변수입니다.
-
-	//Increase();	   
-	//Increase();
-	//Increase();
-	//Increase();
-#pragma endregion
-#pragma endregion
-
-#pragma region 소수
-
-	//// 1. 입력할 수 있는 변수를 선언합니다.
-	//int flag = 0;
-	//int number = 0;
-
-	//// 2. 값을 입력합니다.
-	//scanf_s("%d", &number);
-
-	//// 3. 반복문을 이용해서 소수를 판별합니다.
-	//for (int i = 2; i < number; i++)
+	//if (x < y || y++)
 	//{
-	//	if (number % i == 0)
-	//	{
-	//		flag++;
-	//		break;
-	//	}
+	//	printf("Short Curcuit\n");
 	//}
 
-	//// 4. 내가 입력한 값이 소수인지 아닌지 판별합니다.
-	//if (flag == 0)
-	//{
-	//	printf("소수");
-	//}
-	//else
-	//{
-	//	printf("소수가 아닙니다.");
-	//}
-
+	//printf("y 변수의 값 : %d\n", y);
 
 #pragma endregion
 
-#pragma region 매크로
-	// 프로그램 내에서 특정한 데이터가 문자열로 정의되고
-	// 처리되는 과정입니다.
-
-	printf("SIZE 매크로의 값 : %d\n", SIZE);
-
-	// 매크로의 과정은 컴파일 이전에 실행되며, 각 매크로가
-	// 실행될 때 매크로의 대체 목록을 넣어야 하므로 프로그램의
-	// 크기가 커지게 됩니다. 
-#pragma endregion
 
 
 	return 0;
