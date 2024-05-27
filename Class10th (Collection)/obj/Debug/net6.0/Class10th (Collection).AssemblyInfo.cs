c0759b6ff281d517e4c5f63598442bb91572bcab
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-05-27 15:44:49 +0900

    (2024.5.27) 콜렉션

--- Class10th (Collection)/obj/Debug/net6.0/Class10th (Collection).AssemblyInfo.cs
+++ Class10th (Collection)/obj/Debug/net6.0/Class10th (Collection).AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Class10th (Collection)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f034cc7918b769f8e469d65c1d7579450a2ac83e")]
-[assembly: System.Reflection.AssemblyProductAttribute("Program")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Program")]
+[assembly: System.Reflection.AssemblyProductAttribute("Class10th (Collection)")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Class10th (Collection)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
