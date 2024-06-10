b0e9f34f18065516a57681b864e4077f22a8fabc
Unity2033 <kileab@naver.com>
2024-06-10 19:43:31 +0900

    (2024.6.10) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class SingleLinkedList
+{
+private:
+	int size;
+
+	struct Node
+	{
+		T data;
+		Node * next;
+	};
+
+	Node * head;
+
+public:
+	SingleLinkedList()
+	{
+		size = 0;
+		head = nullptr;
+	}
+
+	void PushFront(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node;
+
+			head->data = data;
+			head->next = nullptr;
+		}
+		else
+		{
+			Node * newNode = new Node;
+
+			newNode->data = data;
+			newNode->next = head;
+
+			head = newNode;
+		}
+
+		size++;
+	}
+
+	void Show()
+	{
+		Node * currentNode = head;
+
+		while (currentNode != nullptr)
+		{
+			cout << currentNode->data << endl;
+
+			currentNode = currentNode->next;
+		}
+	}
+
+
+};
+
+int main()
+{
+  
+}
