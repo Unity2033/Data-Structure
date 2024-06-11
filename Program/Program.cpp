37c293d86f950ea0d354cb0e5613a4ba3d807066
Unity2033 <kileab@naver.com>
2024-06-11 19:45:28 +0900

    (2024.6.11) 양방향 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -45,6 +45,87 @@ public:
 		size++;
 	}
 
+	void PopFront()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		else
+		{
+			Node * deleteNode = head;
+
+			head = deleteNode->next;
+
+			delete deleteNode;
+
+			size--;
+		}
+	}
+
+	void PushBack(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node;
+
+			head->data = data;
+			head->next = nullptr;
+		}
+		else
+		{
+			Node * currentNode = head;
+
+			while (currentNode->next != nullptr)
+			{
+				currentNode = currentNode->next;
+			}
+
+			Node * newNode = new Node;
+
+			currentNode->next = newNode;
+
+			newNode->data = data;
+			newNode->next = nullptr;
+		}
+
+		size++;
+	}
+
+	void PopBack()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		else
+		{
+			Node * deleteNode = head;
+			Node * previousNode = nullptr;
+
+			if (size == 1)
+			{
+				head = deleteNode->next;
+
+				delete deleteNode;
+			}
+			else
+			{
+				while (deleteNode->next != nullptr)
+				{
+					previousNode = deleteNode;
+					deleteNode = deleteNode->next;
+				}
+
+				previousNode->next = deleteNode->next;
+
+				delete deleteNode;
+			}
+
+			size--;
+		}
+	}
+
 	void Show()
 	{
 		Node * currentNode = head;
@@ -62,5 +143,15 @@ public:
 
 int main()
 {
-  
+	SingleLinkedList<int> singleLinkedList;
+
+	singleLinkedList.PushBack(10);
+	singleLinkedList.PushBack(20);
+	singleLinkedList.PushBack(30);
+
+	//singleLinkedList.PopFront();
+
+	singleLinkedList.Show();
+
+	return 0;
 }
