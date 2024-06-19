99098f018dcee261baa3476c01fd2278ebd063c2
Unity2033 <kileab@naver.com>
2024-06-19 19:46:42 +0900

    (2024.6.19) 문자열

---
+++ Class6th (Circle Queue)/Source.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+template <typename T>
+class CircleQueue
+{
+private:
+	int size;
+	int rear;
+	int front;
+
+	T container[SIZE];
+
+public:
+	CircleQueue()
+	{
+		size = 0;
+		rear = SIZE - 1;
+		front = SIZE - 1;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			container[i] = NULL;
+		}
+	}
+
+	void Push(T data)
+	{
+		if (front == (rear + 1) % SIZE)
+		{
+			cout << "CircleQueue Overflow" << endl;
+		}
+		else
+		{
+			rear = (rear + 1) % SIZE;
+			container[rear] = data;
+
+			size++;
+		}
+	}
+
+	void Pop()
+	{
+		if (Empty())
+		{
+			cout << "CircleQueue is Empty" << endl;
+		}
+		else
+		{
+			front = (front + 1) % SIZE;
+
+			container[front] = NULL;
+
+			size--;
+		}
+	}
+
+	bool Empty()
+	{
+		if (front == rear)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	int& Size()
+	{
+		return size;
+	}
+
+	T& Front()
+	{
+		return container[(front + 1) % SIZE];
+	}
+};
+
+int main()
+{
+	CircleQueue<char> circleQueue;
+
+	circleQueue.Push('A');
+	circleQueue.Push('B');
+	circleQueue.Push('C');
+	circleQueue.Push('D');
+
+	while (circleQueue.Empty() == false)
+	{
+		cout << circleQueue.Front() << endl;
+
+		circleQueue.Pop();
+	}
+
+	circleQueue.Push('E');
+	circleQueue.Push('F');
+
+	while (circleQueue.Empty() == false)
+	{
+		cout << circleQueue.Front() << endl;
+
+		circleQueue.Pop();
+	}
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,35 +2,105 @@
 
 using namespace std;
 
-#define SIZE 5
-
 template <typename T>
-class CircleQueue
+class Vector
 {
 private:
 	int size;
-	int rear;
-	int front;
+	int capacity;
 
-	T container[SIZE];
-
+	T * container;
 public:
-
-	CircleQueue()
+	Vector()
 	{
 		size = 0;
-		rear = SIZE - 1;
-		front = SIZE - 1;
+		capacity = 0;
 
-		for (int i = 0; i < SIZE; i++)
+		container = nullptr;
+	}
+
+	void PushBack(T data)
+	{
+		if (capacity <= 0)
 		{
-			container[i] = NULL;
+			Resize(1);
+		}
+		else if (size >= capacity)
+		{
+			Resize(capacity * 2);
+		}
+
+		container[size++] = data;
+	}
+
+	void Resize(int newSize)
+	{
+		// 1. capacity에 새로운 size값을 저장합니다.
+		capacity = newSize;
+
+		// 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
+		//    메모리 공간을 가리키도록 합니다.
+		T * newContainer = new T[capacity];
+
+		// 3. 새로운 메모리 공간의 값을 초기화합니다.
+		for (int i = 0; i < capacity; i++)
+		{
+			newContainer[i] = NULL;
+		}
+
+		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열에
+		//    넣어줍니다.
+		for (int i = 0; i < size; i++)
+		{
+			newContainer[i] = container[i];
+		}				 
+
+		// 5. 기존 배열의 메모리 해제합니다.
+		if (container != nullptr)
+		{
+			delete [] container;
+		}
+
+		// 6. 기존에 배열을 가리키던 포인터 변수의 값을
+		//    새로운 배열의 시작 주소로 가리킵니다.
+		container = newContainer;			  
+	}
+
+	T & operator [] (const int& index)
+	{
+		return container[index];
+	}
+
+	int & Size()
+	{
+		return size;
+	}
+
+	~Vector()
+	{
+		if (container != nullptr)
+		{
+			delete [] container;
 		}
 	}
 };
 
 int main()
 {
+	Vector<int> vector;
+
+	vector.PushBack(10);
+	vector.PushBack(20);
+	vector.PushBack(30);
+
+	for (int i = 0; i < vector.Size(); i++)
+	{
+		cout << vector[i] << endl;
+	}
+
+
+
+
 
 	return 0;
 }
