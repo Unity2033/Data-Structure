e09d81f0c893c61febf9c056d52c886f317eed57
Unity2033 <kileab@naver.com>
2024-06-21 19:45:04 +0900

    (2024.6.21) 힙

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,85 +1,44 @@
 ﻿#include <iostream>
 
-#pragma warnings(disable : 4996)
-
 using namespace std;
 
-class String
+#define SIZE 6
+
+template<typename KEY, typename VALUE>
+class HashTable
 {
 private:
-	int size;
-	char * container;
+	struct Node
+	{
+		KEY key;
+		VALUE value;
+
+		Node * next;
+	};
+
+	struct Bucket
+	{
+		int count;
+		Node * head;
+	};
+
+	Bucket bucket[SIZE];
 
 public:
-	String()
+	HashTable()
 	{
-		size = 0;
-		container = nullptr;
-	}
-
-	void operator = (const char * content)
-	{
-		int arraySize = strlen(content) + 1;
-
-		size = strlen(content);
-
-		if (container == nullptr)
+		for (int i = 0; i < SIZE; i++)
 		{
-			container = new char[arraySize];
-
-			for (int i = 0; i < arraySize; i++)
-			{
-				container[i] = content[i];
-			}
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
 		}
-		else
-		{
-			char * newContainer = new char[arraySize];
 
-			for (int i = 0; i < arraySize; i++)
-			{
-				newContainer[i] = content[i];
-			}
-
-			delete container;
-
-			container = newContainer;
-		}
-	}
-
-	int & Size()
-	{
-		return size;
-	}
-
-	char & operator [] (int index)
-	{
-		return container[index];
 	}
 
 };
 
-
 int main()
 {
-	// String string;
-	// 
-	// string = "Janna";
-	// 
-	// cout << "string의 크기 : " << string.Size() << endl;
-	// 
-	// string = "Alistar";
-	// 
-	// for (int i = 0; i < string.Size(); i++)
-	// {
-	// 	cout << string[i];
-	// }
-
-	string name;
-
-	name = "qe";
-
-	cout << "문자열이 같나요? " << name.compare("qd");
 
 	return 0;
 }
