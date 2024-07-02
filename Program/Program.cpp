ac0801fa350380db5ce38301c4bc4f53f5cd5f7a
Unity2033 <kileab@naver.com>
2024-07-02 19:46:22 +0900

    (2024.7.2) 레드 블랙 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -15,15 +15,148 @@ private:
 	};
 
 	Node * root;
-public:
+public :
+
 	BinarySearchTree()
 	{
 		root = nullptr;
+	}
+
+	Node * Root()
+	{
+		return root;
+	}
+
+	Node * CreateNode(T data)
+	{
+		Node * newNode = new Node();
+
+		newNode->data = data;
+
+		newNode->left = nullptr;
+
+		newNode->right = nullptr;
+
+		return newNode;
+	}
+
+	void Insert(T data)
+	{
+		if (root == nullptr)
+		{
+			root = CreateNode(data);
+		}
+		else
+		{		 
+			Node * currentNode = root;
+
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data == data)
+				{
+					return;
+				}
+				else if (currentNode->data > data)
+				{
+					if (currentNode->left == nullptr)
+					{
+						currentNode->left = CreateNode(data);
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->left;
+					}
+				}
+				else
+				{
+					if (currentNode->right == nullptr)
+					{
+						currentNode->right = CreateNode(data);
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}
+
+			}
+		}
+	}
+
+	bool Find(T data)
+	{
+		Node * currentNode = root;
+
+		if (currentNode == nullptr)
+		{
+			return false;
+		}
+		else
+		{
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data == data)
+				{
+					return true;
+				}
+				else
+				{
+					if (currentNode->data > data)
+					{
+						currentNode = currentNode->left;
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}
+			}
+
+			return false;
+		}
+	}
+
+	void Inorder(Node * root)
+	{
+		if (root != nullptr)
+		{
+			Inorder(root->left);
+			cout << root->data << " ";
+			Inorder(root->right);
+		}
+	}
+
+	void Destroy(Node* root)
+	{
+		if (root != nullptr)
+		{
+			Destroy(root->left);
+			Destroy(root->right);
+			delete root;
+		}
+	}
+
+	~BinarySearchTree()
+	{
+		Destroy(root);
 	}
 };
 
 int main()
 {
+	BinarySearchTree<int> binarySearchTree;
+
+	binarySearchTree.Insert(10);
+	binarySearchTree.Insert(7);
+	binarySearchTree.Insert(15);
+	binarySearchTree.Insert(5);
+
+	cout << binarySearchTree.Find(5) << endl;
+	cout << binarySearchTree.Find(0) << endl;
+
+	binarySearchTree.Inorder(binarySearchTree.Root());
 
 	return 0;
 }
