45c65e6b1276ac5008b45e72221552d18ab7006b
Unity2033 <kileab@naver.com>
2024-08-02 19:44:05 +0900

    (2024.8.2) 데크

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
 
+#define SIZE 26
+
+class Trie
+{
+private:
+	bool final;
+
+	Trie * alphabet[SIZE];
+
+public:
+	Trie()
+	{
+		final = false;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			alphabet[i] = nullptr;
+		}
+	}
+};
+
 int main()
 {
+
+
 	return 0;
 }
