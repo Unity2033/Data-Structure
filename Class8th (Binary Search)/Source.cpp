bcb7f4a60c1e3e3fb54c27b1c50dca53ecc90168
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-12-17 21:42:18 +0900

    (2024.12.17) 이진 탐색

---
+++ Class8th (Binary Search)/Source.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 8
+
+void BinarySearch(int list[], int key)
+{
+	int left = 0;
+	int right = SIZE - 1;
+
+	while (left <= right)
+	{
+		int pivot = (left + right) / 2;
+
+		if (list[pivot] == key)
+		{
+			cout << "Key Found : " << list[pivot] << endl;
+
+			return;
+		}
+		else if (list[pivot] > key)
+		{
+			right = pivot - 1;
+		}
+		else
+		{
+			left = pivot + 1;
+		}
+	}
+
+	cout << "Not Key Found" << endl;
+}
+
+int main()
+{
+#pragma region 이진 탐색
+	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를
+	// 좁혀나가는 방식으로 동작하는 알고리즘입니다.
+
+	int list[SIZE] = { 5,6,11,13,27,55,66,99 };
+
+	BinarySearch(list, 1);
+
+#pragma endregion
+
+
+	return 0;
+}
+
+
---
+++ Class9th (Count Sort)/Source.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 10
+
+int main()
+{
+#pragma region 계수 정렬
+	// 데이터의 값을 비교하지 않고 각 원소에 데이터가
+	// 몇 개 있는지 개수를 세어 저장한 뒤 정렬하는 알고리즘입니다.
+
+	int list[SIZE] = { 1,1,3,1,2,3,4,4,2,5 };
+
+	int count[5] = { 0, };
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		count[list[i] - 1]++;
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (list[i] != 0)
+		{
+			for (int j = 0; j < count[i]; j++)
+			{
+				cout << i + 1 << " ";
+			}
+		}
+	}
+
+#pragma endregion
+
+	return 0;
+}
+
+
